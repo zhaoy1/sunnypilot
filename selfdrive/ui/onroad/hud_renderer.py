d873d0b4b3629a75c616e28779c29979ad10af7a
Zhao <zhaoyubo@gmail.com>
2026-01-15 21:05:41 -0800

    Remove leftover test buttons from _draw_current_speed

--- selfdrive/ui/onroad/hud_renderer.py
+++ selfdrive/ui/onroad/hud_renderer.py
@@ -182,14 +182,6 @@ class HudRenderer(Widget):
     speed_pos = rl.Vector2(rect.x + rect.width / 2 - speed_text_size.x / 2, 180 - speed_text_size.y / 2)
     rl.draw_text_ex(self._font_bold, speed_text, speed_pos, FONT_SIZES.current_speed, 0, COLORS.white)
 
-    # Draw + and - buttons next to the speed
-    button_y = 180
-    plus_x = rect.x + 100
-    minus_x = rect.x + rect.width - 300
-
-    rl.draw_text_ex(self._font_bold, "+", rl.Vector2(plus_x, button_y), 120, 0, COLORS.white)
-    rl.draw_text_ex(self._font_bold, "-", rl.Vector2(minus_x, button_y), 120, 0, COLORS.white)
-
     unit_text = "km/h" if ui_state.is_metric else "mph"
     unit_text_size = measure_text_cached(self._font_medium, unit_text, FONT_SIZES.speed_unit)
     unit_pos = rl.Vector2(rect.x + rect.width / 2 - unit_text_size.x / 2, 290 - unit_text_size.y / 2)
