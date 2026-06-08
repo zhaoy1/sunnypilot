ae0fe8219f8dad9c1469d1282cd316db0884a151
Zhao <zhaoyubo@gmail.com>
2026-06-07 21:24:54 -0700

    Revert pcmCruise change for Rivian - must stay True for stock ACM enable

--- opendbc_repo/opendbc/car/rivian/interface.py
+++ opendbc_repo/opendbc/car/rivian/interface.py
@@ -32,7 +32,6 @@ class CarInterface(CarInterfaceBase):
     ret.alphaLongitudinalAvailable = True
     if alpha_long:
       ret.openpilotLongitudinalControl = True
-      ret.pcmCruise = False
       ret.safetyConfigs[0].safetyParam |= RivianSafetyFlags.LONG_CONTROL.value
 
     ret.longitudinalActuatorDelay = 0.35
@@ -57,7 +56,6 @@ class CarInterface(CarInterfaceBase):
 
     if alpha_long and stock_cp.alphaLongitudinalAvailable:
       stock_cp.openpilotLongitudinalControl = True
-      stock_cp.pcmCruise = False
       stock_cp.safetyConfigs[0].safetyParam |= RivianSafetyFlags.LONG_CONTROL.value
 
     return ret
